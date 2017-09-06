b8ab2a93cc5f3a6050ef541ece0ab8815ea1133c
luckalway <luckalway@126.com>
2017-09-06 18:49:41 +0800

    api

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var path = require('path');
 var fs = require("fs");
 
+var baseUploadUrl = CONF.baseUploadUrl;
+
 router.get('/messages', function(req, res, next) {
-	couchdb.view("messages", "message", function(err, body) {
-		console.log(err);
+	couchdb.view("messages", "by_message_id", function(err, body) {
 		if (!err) {
 			var docs = [];
-			console.log(docs);
 			body.rows.forEach(function(doc) {
 				docs.push(doc.value);
 			});
@@ -18,12 +18,33 @@ router.get('/messages', function(req, res, next) {
 	});
 });
 
-router.get('/messages/:id', function(req, res, next) {
-	couchdb.get(req.params.id, {
-		revs_info : true
+router.get('/messages/:id/videos', function(req, res, next) {
+	couchdb.view("messages", "by_message_id", {
+		startkey : [ req.params.id ],
+		endkey : [ req.params.id, {} ]
 	}, function(err, body) {
-		res.send(body);
-		res.status(200).end();
+		if (!err) {
+			if (body.rows.length == 0) {
+				res.send([]);
+				res.status(200).end();
+			}
+
+			var message = body.rows[0].value;
+			var messageVideos = [];
+			for (var i = 1; i < body.rows.length; i++) {
+				var part = body.rows[i].value;
+				messageVideos.push({
+					id :  part._id,
+					videoUrl : baseUploadUrl + part.video.url,
+					title : message.title + '(' + part.partNo + '/'
+							+ message.countOfParts + ')',
+					date:message.date
+				});
+			}
+
+			res.send(messageVideos);
+			res.status(200).end();
+		}
 	});
 });
 
