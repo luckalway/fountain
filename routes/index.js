a8a72d75b4f607a877af0e1684fe84b7a0083f6e
luckalway <luckalway@126.com>
2017-10-01 22:06:45 +0800

    req.query.echostr

--- routes/index.js
+++ routes/index.js
@@ -2,3 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = router;
+
+
+router.get('/', function(req, res, next) {
+	res.send(req.query.echostr);
+	res.status(200).end();
+});
